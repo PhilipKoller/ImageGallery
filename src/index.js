4e595007243a273dbbe5f1681a510ad4b836faca
Philip Koller <philip.koller@outlook.com>
2022-10-24 16:45:21 -0500

    added style-loader setup webpack for css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require('file-loader?name=[name].[ext]!./index.html');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import './App.css'
 
 const appElement = document.getElementById('app');
 
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                        "style-loader",
+                         "css-loader"
+                     ],
             }
         ]
     }
