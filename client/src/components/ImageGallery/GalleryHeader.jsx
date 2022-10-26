6e6cf171c822eb9351059160f8c3ba3c14de3de1
Philip Koller <philip.koller@outlook.com>
2022-10-26 17:13:39 -0500

    connected to MongoDB atlas and reconfigured post req to use image.model

--- client/src/components/ImageGallery/GalleryHeader.jsx
+++ client/src/components/ImageGallery/GalleryHeader.jsx
@@ -5,7 +5,7 @@ const GalleryHeader = ({handleImageUpload}) => {
     return (
         <form id="form">
             <label>Select Photo</label><br />
-            <input type="file" name="image" accept="image/*"></input>
+            <input type="file" name="image" accept="image/*" id="imageInput"></input>
             <button type="submit" onClick={(e) => { handleImageUpload(e) }}>Submit</button>
         </form>
     )
--- server/server.js
+++ server/server.js
@@ -1,31 +1,62 @@
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const ImageModel = require('./image.model')
+
 const app = express();
 const path = require('path');
+const { urlencoded } = require('express');
 const port = 3000;
 
 
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+mongoose.connect(process.env.KEY)
+    .then(() => {
+        console.log('mongo atlas connected')
+    })
+    .catch((error) => {
+        console.log('Error: ', error)
+    })
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        console.log(req.file)
-        cb(null, 'images')
-    },
+    destination: "uploaded_images",
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
+        cb(null, file.originalname)
     }
 })
 
 
-const upload = multer({storage: storage});
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.use(express.json());
+const upload = multer({storage: storage}).single('image');
 
 
-app.post('/upload', upload.single('image'), (req, res) => {
-    console.log(req.file)
-    res.send('got it');
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            const newImage = new ImageModel({
+                name: req.file.filename,
+                image: {
+                    data: req.file.filename,
+                    contentType: 'image/png'
+                }
+            })
+            newImage.save()
+            .then(() => {
+                res.send('uploaded')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        }
+    })
 })
 
 app.listen(port, () => {
